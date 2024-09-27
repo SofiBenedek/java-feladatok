bf745beef40c4a341c8c7f05c6b25ab93221a3cf
SofiBenedek <98951608+SofiBenedek@users.noreply.github.com>
2024-09-27 11:33:15 +0200

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,6 @@
+let tomeg = 81.2;
+let magassag = 1.78;
+
+let BMI = tomeg/magassag**2;
+
+console.log(BMI);
---
+++ java2.js
@@ -0,0 +1,11 @@
+let a = 10.4;
+let b = 13.5;
+let c = 8.2;
+
+let felszin = a*b*c;
+
+console.log("Felszine: ", felszin)
+
+let terfogat = 2*(a*b+b*c+c*a);
+
+console.log("Térfogat: ", terfogat)
